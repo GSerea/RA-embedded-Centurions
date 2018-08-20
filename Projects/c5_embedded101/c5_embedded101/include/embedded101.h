eb9f2d8de63778012011799a912b517fb4d5eeb6
Zaharia <zaharia.mihai@outlook.com>
2018-08-20 20:56:14 +0300

    Merge remote-tracking branch 'upstream/master'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- Projects/c5_embedded101/c5_embedded101/include/embedded101.h
+++ Projects/c5_embedded101/c5_embedded101/include/embedded101.h
@@ -48,10 +48,16 @@ typedef struct {
 
 
 #define PTC12		(12U)								/* Button 1 position */
+#define PTC13		(13U)								/* Button 2 position */
 #define PTD15		(15U)								/* Red LED position */
+#define PTD16		(16U)								/* Red LED position */
+#define PTD0		(0U)
 
 #define BTN_1		((uint32_t)(1<<PTC12))				/* Button 1 bitmask */
+#define BTN_2		((uint32_t)(1<<PTC13))				/* Button 1 bitmask */
 #define LED_RED		((uint32_t)(1<<PTD15))				/* Red LED bitmask */
+#define LED_BLUE	((uint32_t)(1<<PTD0))				/* BLUE LED bitmask */
+#define LED_GREEN	((uint32_t)(1<<PTD16))				/* GREEN LED bitmask */
 
 
 typedef struct {
--- Projects/c5_embedded101/c5_embedded101/src/hello.c
+++ Projects/c5_embedded101/c5_embedded101/src/hello.c
@@ -27,9 +27,13 @@ int main(void) {
 
   // 2) Pin Direction ---> PDDR 1 << x
   (((GPIO_map_tp) PORT_D)->PDDR) |= LED_RED;
+  (((GPIO_map_tp) PORT_D)->PDDR) |= LED_BLUE;
+  (((GPIO_map_tp) PORT_D)->PDDR) |= LED_GREEN;
 
   // 3) Port function ---> PCR.Mux : 001b = GPIO
   PCR_PORT_D[PTD15] |= PCR_GPIO_MODE_MASK;
+  PCR_PORT_D[PTD0] |= PCR_GPIO_MODE_MASK;
+  PCR_PORT_D[PTD16] |= PCR_GPIO_MODE_MASK;
 
   // 4) Turn on LED
   ((GPIO_map_tp) PORT_D)->PCOR |= LED_RED;
@@ -48,19 +52,34 @@ int main(void) {
   *(PCC_PORT_C) |= PCC_ENABLE_PORT_MASK;
 
   //2)Pin Direction --->PDDR 0<<x
-  (((GPIO_map_tp) PORT_C)->PDDR) &= ~(1<<12U);
+  (((GPIO_map_tp) PORT_C)->PDDR) &= ~(1<<12U + 1<<13U);
 
   //3)Port function ---> PCR.Mux :001b=GPIO
   PCR_PORT_C[12U] |= PCR_GPIO_MODE_MASK;
+  PCR_PORT_C[13U] |= PCR_GPIO_MODE_MASK;
 
 
   for(;;) {
 
 	  ((GPIO_map_tp) PORT_D)->PSOR |= LED_RED;
+	  ((GPIO_map_tp) PORT_D)->PSOR |= LED_BLUE;
+	  ((GPIO_map_tp) PORT_D)->PSOR |= LED_GREEN;
 
 	  if((((GPIO_map_tp) PORT_C)->PDIR) & (1<<12))
 	  {
-		  ((GPIO_map_tp) PORT_D)->PCOR |= LED_RED;
+		  if((((GPIO_map_tp) PORT_C)->PDIR) & (1<<13)){
+			  ((GPIO_map_tp) PORT_D)->PCOR |= LED_GREEN;
+		  }
+		  else
+		  {
+		  ((GPIO_map_tp) PORT_D)->PCOR |= LED_BLUE;
+		  }
+	  }
+	  else{
+		  if((((GPIO_map_tp) PORT_C)->PDIR) & (1<<13))
+	  	  {
+	  		  ((GPIO_map_tp) PORT_D)->PCOR |= LED_RED;
+	  	  }
 	  }
 
 
@@ -92,29 +111,3 @@ int main(void) {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
